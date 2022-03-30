7844ed6d0efafe9dc41074a3476f0cf199299367
James Thomson <101159512+JimmieThomson@users.noreply.github.com>
2022-03-30 13:29:27 +1100

    SwiftUI
    Base Swift UI

---
+++ Wednesday/Wednesday/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wednesday
+//
+//  Created by James Thomson on 30/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wednesday/Wednesday/WednesdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WednesdayApp.swift
+//  Wednesday
+//
+//  Created by James Thomson on 30/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct WednesdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
